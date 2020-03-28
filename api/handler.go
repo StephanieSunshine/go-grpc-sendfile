3caef53dcaa6a0f22caa1eb5db34ca8475c73627
Stephanie Sunshine <ponyosunshine@gmail.com>
2020-03-28 15:19:42 -0700

    Template relics that needed cleanup

--- api/handler.go
+++ api/handler.go
@@ -11,6 +11,6 @@ type Server struct {
 
 // SayHello generates response to a Ping request
 func (s *Server) Send(ctx context.Context, in *FileRequestMessage) (*FileResponseMessage, error) {
-  log.Printf("Receive message %s", in.Greeting)
+	log.Println("Receive message: ", in)
   return &FileResponseMessage{}, nil
 }
